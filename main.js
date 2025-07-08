1f5382e304683400433e4e916be57283600f5265
kuga <esixterb58@gmail.com>
2025-07-08 17:18:24 +0300

    feat: create cards to console

---
+++ main.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 1,
+    title: "Смартфон Galaxy X",
+    price: 29999,
+    description: "Мощный смартфон с AMOLED-дисплеем и 3 камерами.",
+  },
+  {
+    id: 2,
+    title: "Ноутбук TechBook 15",
+    price: 64999,
+    description: "Универсальный ноутбук для работы и развлечений.",
+  },
+  {
+    id: 3,
+    title: "Наушники NoiseOff",
+    price: 4999,
+    description: "Беспроводные наушники с шумоподавлением.",
+  },
+]
+
+// Затем с помощью JS пройдитесь по массиву products с помощью forEach или цикла и для каждого товара создайте DOM-элементы
+products.forEach(goods => {
+  const divCard = document.createElement('div')
+  divCard.classList.add('card', 'data-id', 'data-price')
+
+  const divTitle = document.createElement('div')
+  divTitle.classList.add('title')
+  divTitle.textContent = goods.title
+  
+  const divPrice = document.createElement('div')
+  divPrice.classList.add('price')
+  divPrice.textContent = goods.price + ' ₽'
+
+  const divDescription = document.createElement('div')
+  divDescription.classList.add('description')
+  divDescription.textContent = goods.description
+
+
+  divCard.append(divTitle, divPrice, divDescription)
+  
+  console.log(goods)
+  console.log(divCard)
+})
